544029b5bbcd8e1218819d3382aae1ccbe216868
yonjinoh <2020110869@dgu.ac.kr>
2023-09-28 22:15:48 +0900

    oss05.py 과제 5번

--- oss05.py
+++ oss05.py
@@ -1,4 +1,5 @@
 name = input("이름을 입력해주세요: ")
 num = input("학번을 입력해주세요: ")
+depart = input("학과를 입력해주세요: ")
 
-print("<출력>\n이름 :", name, "\n학번 :", num)
+print("<출력>\n이름 :", name, "\n학번 :", num, "\n학과 :", depart)
